dc087caf8a962aebec640a9be859f8e96e452ebe
joaquimpfilho <joaquimp@gmail.com>
2015-03-16 21:11:20 -0300

    Initial Commit

---
+++ Persistencia_Realm.io/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Persistencia_Realm.io
+//
+//  Created by joaquim on 16/03/15.
+//  Copyright (c) 2015 joaquim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Persistencia_Realm.io/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Persistencia_Realm.io
+//
+//  Created by joaquim on 16/03/15.
+//  Copyright (c) 2015 joaquim. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
